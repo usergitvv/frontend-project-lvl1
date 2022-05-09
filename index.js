65ea74966292514ef8ac565a3a99ddc3755cd97c
usergitvv <valery.voronkov@yandex.ru>
2022-05-09 20:32:57 +0500

    Corrections in index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 export const randomNum = (min, max) => {
-    const minNum = Math.ceil(min);
-    const maxNum = Math.floor(max);
-    const rand = minNum + (Math.random() * (maxNum + 1 - minNum));
-    return Math.floor(rand);
+  const minNum = Math.ceil(min);
+  const maxNum = Math.floor(max);
+  const rand = minNum + (Math.random() * (maxNum + 1 - minNum));
+  return Math.floor(rand);
 };
 
 export const randomIndex = (arr) => {
-    const rand = Math.floor(Math.random() * arr.length);
-    return arr[rand];
+  const rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
 };
 
 export const randomIndexTwo = (arr) => {
-    const rand = Math.floor(Math.random() * arr.length);
-    return rand;
+  const rand = Math.floor(Math.random() * arr.length);
+  return rand;
 };
